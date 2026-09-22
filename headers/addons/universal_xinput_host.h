4ea8af918ddfd9a6aa8675507f5c128747994598
chanchella <3morichanchellagemi@gmail.com>
2026-09-22 20:26:24 +0300

    G1A: add isolated universal XInput host addon

---
+++ headers/addons/universal_xinput_host.h
@@ -0,0 +1,54 @@
+#ifndef _UNIVERSAL_XINPUT_HOST_H_
+#define _UNIVERSAL_XINPUT_HOST_H_
+
+#include "gpaddon.h"
+#include "usblistener.h"
+#include "gamepad.h"
+
+#ifndef UNIVERSAL_XINPUT_HOST_ENABLED
+#define UNIVERSAL_XINPUT_HOST_ENABLED 0
+#endif
+
+class UniversalXInputHostAddon : public GPAddon, public USBListener {
+public:
+    bool available() override;
+    void setup() override;
+    void preprocess() override;
+    void process() override {}
+    void postprocess(bool sent) override {}
+    void reinit() override {}
+    std::string name() override { return "UniversalXInputHost"; }
+    USBListener* getListener() override { return this; }
+
+    void mount(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_report, uint16_t desc_len) override {}
+    void xmount(uint8_t dev_addr, uint8_t instance, uint8_t controllerType, uint8_t subtype) override;
+    void unmount(uint8_t dev_addr) override;
+    void report_received(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len) override;
+    void report_sent(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len) override {}
+    void set_report_complete(uint8_t dev_addr, uint8_t instance, uint8_t report_id, uint8_t report_type, uint16_t len) override {}
+    void get_report_complete(uint8_t dev_addr, uint8_t instance, uint8_t report_id, uint8_t report_type, uint16_t len) override {}
+
+private:
+    static constexpr uint8_t SLOT_COUNT = 3;
+
+    struct XInputSlot {
+        bool mounted = false;
+        bool hasReport = false;
+        uint8_t devAddr = 0;
+        uint8_t instance = 0;
+        uint8_t subtype = 0;
+        GamepadState state {};
+    };
+
+    XInputSlot slots[SLOT_COUNT];
+
+    void resetSlot(uint8_t slot);
+    int8_t findSlot(uint8_t devAddr, uint8_t instance) const;
+    int8_t allocateSlot(uint8_t devAddr, uint8_t instance, uint8_t subtype);
+
+    static uint16_t axisX(int16_t value);
+    static uint16_t axisY(int16_t value);
+    static bool parseXbox360Report(GamepadState& out, uint8_t const* report, uint16_t len);
+};
+
+#endif
