48d643589461ed4449d0217f036b233a33adefed
chanchella <3morichanchellagemi@gmail.com>
2026-09-23 01:26:03 +0300

    G2C1: load universal HID gamepad host

--- src/gp2040.cpp
+++ src/gp2040.cpp
@@ -30,6 +30,7 @@
 #include "addons/i2c_gpio_pcf8575.h"
 #include "addons/gamepad_usb_host.h"
 #include "addons/universal_xinput_host.h"
+#include "addons/universal_hid_gamepad_host.h"
 #include "addons/universal_output_router.h"
 #include "addons/he_trigger.h"
 #include "addons/tg16_input.h"
@@ -109,6 +110,7 @@ void GP2040::setup() {
 	addons.LoadUSBAddon(new KeyboardHostAddon());
 	addons.LoadUSBAddon(new GamepadUSBHostAddon());
 	addons.LoadUSBAddon(new UniversalXInputHostAddon());
+	addons.LoadUSBAddon(new UniversalHIDGamepadHostAddon());
 	addons.LoadAddon(new UniversalOutputRouterAddon());
 	addons.LoadAddon(new AnalogInput());
 	addons.LoadAddon(new HETriggerAddon());
