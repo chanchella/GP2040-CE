5b82a8853742193d3cc8d718d1414b902726a6f5
chanchella <3morichanchellagemi@gmail.com>
2026-09-22 22:58:28 +0300

    OAG: apply Vortex product strings to ps4

--- headers/drivers/ps4/PS4Descriptors.h
+++ headers/drivers/ps4/PS4Descriptors.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include "device/oag_identity.h"
+
 #include <stdint.h>
 
 #define PS4_ENDPOINT_SIZE 64
@@ -419,8 +421,8 @@ typedef struct __attribute__((packed)) {
 } PS4Report;
 
 static const uint8_t ps4_string_language[]     = { 0x09, 0x04 };
-static const uint8_t ps4_string_manufacturer[] = "Open Stick Community";
-static const uint8_t ps4_string_product[]      = "GP2040-CE (PS4)";
+static const uint8_t ps4_string_manufacturer[] = OAG_USB_MANUFACTURER_STRING;
+static const uint8_t ps4_string_product[]      = OAG_USB_PRODUCT_STRING;
 static const uint8_t ps4_string_version[]      = "1.0";
 
 static const uint8_t *ps4_string_descriptors[] __attribute__((unused)) =
