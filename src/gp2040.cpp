45b32c65d13c16325e7732fc2d78c95cfc0740aa
chanchella <3morichanchellagemi@gmail.com>
2026-09-22 20:26:29 +0300

    G1A: register universal XInput host addon

--- src/gp2040.cpp
+++ src/gp2040.cpp
@@ -29,6 +29,7 @@
 #include "addons/rotaryencoder.h"
 #include "addons/i2c_gpio_pcf8575.h"
 #include "addons/gamepad_usb_host.h"
+#include "addons/universal_xinput_host.h"
 #include "addons/he_trigger.h"
 #include "addons/tg16_input.h"
 
@@ -106,6 +107,7 @@ void GP2040::setup() {
 	// Setup Add-ons
 	addons.LoadUSBAddon(new KeyboardHostAddon());
 	addons.LoadUSBAddon(new GamepadUSBHostAddon());
+	addons.LoadUSBAddon(new UniversalXInputHostAddon());
 	addons.LoadAddon(new AnalogInput());
 	addons.LoadAddon(new HETriggerAddon());
 	addons.LoadAddon(new BootselButtonAddon());
