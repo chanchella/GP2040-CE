21db79ee7865cba0b44a517784e5f535c3699f50
chanchella <3morichanchellagemi@gmail.com>
2026-09-24 15:20:19 +0300

    fix(oag): keep output routing C++17 compatible

--- oag/src/output/universal_output_mode.cpp
+++ oag/src/output/universal_output_mode.cpp
@@ -5,55 +5,20 @@ namespace oag {
 UniversalOutputRouting UniversalOutputModeRouter::routing() const {
     switch (mode_) {
         case UniversalOutputMode::Touchscreen:
-            return {
-                .gamepad = false,
-                .touchscreen = true,
-                .penTablet = false,
-                .keyboard = false,
-                .mouse = false,
-                .bluetoothPeripheral = true,
-            };
+            return {false, true, false, false, false, true};
 
         case UniversalOutputMode::DrawingTablet:
-            return {
-                .gamepad = false,
-                .touchscreen = false,
-                .penTablet = true,
-                .keyboard = false,
-                .mouse = false,
-                .bluetoothPeripheral = true,
-            };
+            return {false, false, true, false, false, true};
 
         case UniversalOutputMode::MouseKeyboard:
-            return {
-                .gamepad = false,
-                .touchscreen = false,
-                .penTablet = false,
-                .keyboard = true,
-                .mouse = true,
-                .bluetoothPeripheral = true,
-            };
+            return {false, false, false, true, true, true};
 
         case UniversalOutputMode::Hybrid:
-            return {
-                .gamepad = true,
-                .touchscreen = true,
-                .penTablet = true,
-                .keyboard = true,
-                .mouse = true,
-                .bluetoothPeripheral = true,
-            };
+            return {true, true, true, true, true, true};
 
         case UniversalOutputMode::Gamepad:
         default:
-            return {
-                .gamepad = true,
-                .touchscreen = false,
-                .penTablet = false,
-                .keyboard = false,
-                .mouse = false,
-                .bluetoothPeripheral = true,
-            };
+            return {true, false, false, false, false, true};
     }
 }
 
